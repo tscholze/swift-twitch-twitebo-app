859349b7aa148df00afed76cdd087b1f5fe85852
Tobias Scholze <t.scholze@anfe.ma>
2019-04-12 14:28:18 +0200

    Added color set

---
+++ Twitebo/Classes/Helper/Extensions/UIColor+Colors.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Colors.swift
+//  Twitebo
+//
+//  Created by Tobias Scholze on 12.04.19.
+//  Copyright © 2019 Tobias Scholze. All rights reserved.
+//
+
+import UIKit
+
+/// Color set is based on:
+/// https://colorpalettes.net/color-palette-2661/
+extension UIColor
+{
+    /// Brand color.
+    static let brand = UIColor(named: "brand")
+}
